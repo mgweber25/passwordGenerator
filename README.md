239b3042c1ab00b93a2f845956cddc59688aa776
mgweber25 <mike@mgweber.com>
2020-07-21 22:46:55 -0500

    update intial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password Generator Starter Code
+
+
+Site is published @ https://mgweber25.github.io/passwordGenerator/
+
--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
 
 }
