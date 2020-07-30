018ac162cedcb23f403c6135c2db5161f3d51d27
mgweber25 <mike@mgweber.com>
2020-07-30 17:50:14 -0500

    update

--- js/script.js
+++ js/script.js
@@ -70,7 +70,6 @@ if(passChars > 7 && passChars < 129) {
 
 // Get references to the #generate element
 
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
