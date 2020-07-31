8c7d30b66db9dc43da2c54380ee528d684094209
mgweber25 <mike@mgweber.com>
2020-07-31 02:02:01 -0500

    update password js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,4 @@
-// Assignment code here
-
-
-function createPassword () {
-
-
-var numberCheck = document.createElement("box1").innerHTML;
-//box1.setAttribute("type", "checkbox");
-
-var lowerCaseCheck = document.createElement("box2").innerHTML;
-//box2.setAttribute("type", "checkbox");
-
-var upperCaseCheck = document.createElement("box3").innerHTML;
-//box3.setAttribute("type", "checkbox");
-
-var specialCharCheck = document.createElement("box4").innerHTML;
-//box4.setAttribute("type", "checkbox");
-
-
-if (numberCheck === false && 
-    lowerCaseCheck === false && 
-    upperCaseCheck === false && 
-    specialCaseCheck === false) 
-    {
-    alert("You must select one or more attributes to create a password!");
-    exit();
-}
-
-
-
-
-
+// Character arrays & Password Length
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -39,21 +8,96 @@ var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var specialChar = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '-', '`', '{', '}', '|', '~'];
 
+var length = document.querySelector("#pass-length");
+
+
+
+// Generate Password Function
+
+function generatePassword () {
+
+
+      var numberCheck = document.getElementsByTagName("box1")[0].setAttribute(Boolean, "checkbox");
+      //box1
+
+      var lowerCaseCheck = document.getElementsByTagName("box2")[1].setAttribute(Boolean, "checkbox");
+      //box2.setAttribute("type", "checkbox");
+
+      var upperCaseCheck = document.getElementsByTagName("box3")[2].setAttribute(Boolean, "checkbox");
+      //box3.setAttribute("type", "checkbox");
+
+      var specialCharCheck = document.getElementsByTagName("box4")[3].setAttribute(Boolean, "checkbox");
+      //box4.setAttribute("type", "checkbox");
+
+      if (numberCheck === false && 
+        lowerCaseCheck === false && 
+        upperCaseCheck === false && 
+        specialCharCheck === false) 
+        {
+        alert("You must select one or more attributes to create a password!");
+        return;
+      }
+
+
+
+        var charInput = parseInt(document.querySelector("#pass-length"));
+
+
+          
+        
+        if(charInput < 8 || charInput > 128) {
+          alert("Please re-enter a number between 8 and 256 for the password length");
+        }
+        else if(isNaN(charInput) === true) {
+
+            alert("Length of password must be entered as a number only");
+          }
+          
+        
+
+
+      // Object to store user input
+
+      var passwordOptions = {
+        charInput: charInput,
+        numberCheck: numberCheck,
+        lowerCaseCheck: lowerCaseCheck,
+        upperCaseCheck: upperCaseCheck,
+        specialCharCheck: specialCharCheck
+      };
+
+      return passwordOptions;
+}
+
+
+
+
+// Get references to the #generate element
+var createPassword = document.querySelector("#passwordButton");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+createPassword.addEventListener('click', writePassword());
+
+
+
+
+
 
 
 
 
   /*  alert("You must select at least one attribute") */
 
-  var charInput = document.querySelector("pass-length");
 
-var passChars = parseInt(charInput);
-  
-
-if(passChars < 8 || passChars > 128) {
-  alert("Please re-enter a number between 8 and 256 for the password length");
-  
-}
 
 
 
@@ -68,23 +112,3 @@ if(passChars > 7 && passChars < 129) {
 
 }
 
-// Get references to the #generate element
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-
-generateBtn.addEventListener("click", writePassword);
-
-
-}
